244f5d4e74d0799643f944fcb6f533fde828bd2a
zfoltz <62525530+zfoltz@users.noreply.github.com>
2023-04-03 19:17:29 -0600

    Added remaining unguessed letter reveal after game loss & devMode setting for clearing cache (#14)
    * added remaining unguessed letter reveal after game loss
    
    * fixed the clear storage button so that it only shows when running in dev mode
    
    * Fixed game date origin, prompt index, and art index
    
    * Changed v-html block to v-if for trash can per requested changes
    
    * removed old logic from initializeGame method

--- descraibe-frontend/src/App.vue
+++ descraibe-frontend/src/App.vue
@@ -15,12 +15,12 @@
             </path>
           </svg>
         </button>
-        <!-- <button type="button" @click="clearStorage()" class="menu-button">
+        <button v-if="devMode" type="button" @click="clearStorage()" class="menu-button">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
             <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
             <path fill="var(--color-tone-1)" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
           </svg>
-        </button> -->
+        </button>
       </div>
     </div>
   </header>
@@ -164,6 +164,9 @@ export default {
     art() {
       let promptIndex = this.getToday() - this.gameDateOrigin
       return "/" + this.promptMap[promptIndex].image
+    },
+    devMode() {
+      return import.meta.env.MODE === 'development'
     }
   },
   data() {
@@ -190,7 +193,7 @@ export default {
       },
       showOverlay: false,
       gameDateOrigin: 19344,
-      timeToNextGame: "--:--:--"
+      timeToNextGame: "--:--:--",
     }
   },
   mounted() {
@@ -252,6 +255,9 @@ export default {
           let revealChar = document.querySelectorAll("div.letter")[i]
           revealChar.dataset.valueset = "true"
           revealChar.textContent = char
+          if (this.checkGameLost()) {
+            revealChar.style.color= "hsl(1, 0%, 50%)";
+          }
         }
       }
     },
@@ -286,6 +292,11 @@ export default {
       this.gameData.active = false
       this.gameData.timeTaken = Number(this.secondsTaken) + (Number(this.minutesTaken) * 60)
       this.stopTimer()
+      const lettersNotGuessed = [...this.prompt].filter( char => !this.gameData.guesses.includes(char));
+      for (const letterIndex in lettersNotGuessed) {
+        this.revealLetters(lettersNotGuessed[letterIndex]);
+      }
+      
 
       // Add game to game history
       this.gameData.history.push({
